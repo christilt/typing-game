a9dd05433b86a627f567152fbbcc61878c827f63
Chris Tilt <tiltchris+gh@gmail.com>
2023-09-18 20:49:27 +0100

    Add player prefab with movement

--- TypingGame/Assets/Characters/Player/PlayerMovement.cs
+++ TypingGame/Assets/Characters/Player/PlayerMovement.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInput input;
+
+    [SerializeField] private Tilemap navigableTiles;
+    [SerializeField] private Tilemap nonNavigableTiles;
+
+    private void Awake()
     {
-        
+        input = new();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    private void Start()
+    {
+        input.Main.Movement.performed += context => Move(context.ReadValue<Vector2>());
+    }
+
+    private void Move(Vector2 direction)
+    {
+        if (CanMove(direction))
+        {
+            transform.position += (Vector3)direction;
+        }
+    }
+
+    private bool CanMove(Vector2 direction)
+    {
+        var gridPosition = navigableTiles.WorldToCell(transform.position + (Vector3)direction);
+
+        return navigableTiles.HasTile(gridPosition) 
+            && !nonNavigableTiles.HasTile(gridPosition);
     }
 }
