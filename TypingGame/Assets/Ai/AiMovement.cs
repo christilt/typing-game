60c9b6c510bacc852041253b13d26773e0a4f413
Chris Tilt <tiltchris+gh@gmail.com>
2023-09-26 21:54:44 +0100

    Have AI movement based on the centre of tiles

--- TypingGame/Assets/Ai/AiMovement.cs
+++ TypingGame/Assets/Ai/AiMovement.cs
@@ -13,18 +13,19 @@ public class AiMovement : MonoBehaviour
     [SerializeField] private float _movesPerSecond; // TODO: Have this in WPM
     [SerializeField] private Tilemap _allowedTiles;
     [SerializeField] private LayerMask _colliderLayers;
+    [SerializeField] private Transform _centre;
 
     private HashSet<Vector2Int> _allowedPositions;
 
-    private Vector2[] _previousDirectionOptions;
-    private Vector2 _previousDirection = Vector2.zero;
-    private Vector2 _direction = Vector2.zero;
-    private HashSet<Vector2> _collisionDirections = new();
+    private Vector2Int[] _previousDirectionOptions;
+    private Vector2Int _previousDirection = Vector2Int.zero;
+    private Vector2Int _direction = Vector2Int.zero;
+    private HashSet<Vector2Int> _collisionDirections = new();
 
-    private Movement? _worldMovement;
+    private Movement? _centreMovement;
 
-    private Vector2Int CellPosition => ToVector2Int(transform.position);
-    private Vector2 PreviousDirectionOpposite => _previousDirection * -1;
+    private Vector2Int CellPosition => GetCell(_centre.position);
+    private Vector2Int PreviousDirectionOpposite => _previousDirection * -1;
 
     void Awake()
     {
@@ -33,13 +34,17 @@ public class AiMovement : MonoBehaviour
 
     private void Update()
     {
-        if (_worldMovement is null || _worldMovement.Value.IsExceededBy(transform.position) || _collisionDirections.Count > 0)
+        if (_centreMovement is null || _centreMovement.Value.IsExceededBy(_centre.position) || _collisionDirections.Count > 0)
         {
             UpdateDirections();
-            var nextMovePosition2 = CellPosition + _direction;
-            _worldMovement = new Movement(transform.position, nextMovePosition2);
+            if (!TryGetNeighbourCell(CellPosition, _direction, out var neighbourCell))
+            {
+                Debug.LogError($"Could not get neighbour of cell {CellPosition} in direction {_direction}!");
+                return;
+            }
+            _centreMovement = new Movement(_centre.position, GetCellCentre(neighbourCell));
         }
-        transform.position += (_worldMovement.Value.Direction * Time.deltaTime * _movesPerSecond);
+        transform.position += (_centreMovement.Value.Direction * Time.deltaTime * _movesPerSecond);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -49,13 +54,13 @@ public class AiMovement : MonoBehaviour
             return;
 
         _collisionDirections.Add(_direction);
-        var colliderDirection = MapPositionToDirection(collision.gameObject.transform.position);
+        var colliderDirection = GetDirectionToPosition(collision.gameObject.transform.position);
         _collisionDirections.Add(colliderDirection);
     }
 
     private void UpdateDirections()
     {
-        Vector2[] directionOptions = GetDirectionOptions();
+        Vector2Int[] directionOptions = GetDirectionOptions();
         var shouldChooseDirection = !IsPreviousDirectionAvailable(directionOptions) || IsNewDirectionAvailable(directionOptions);
         if (shouldChooseDirection)
         {
@@ -70,16 +75,16 @@ public class AiMovement : MonoBehaviour
         _collisionDirections.Clear();
     }
 
-    private Vector2[] GetDirectionOptions()
+    private Vector2Int[] GetDirectionOptions()
     {
-        return GetNeighboursOf(ToVector2Int(transform.position))
-            .Select(MapPositionToDirection)
+        return GetNeighbourCells(_centre.position)
+            .Select(GetDirectionToCell)
             .Where(d => d != PreviousDirectionOpposite)
             .Except(_collisionDirections)
             .ToArray();
     }
 
-    private Vector2 ChooseDirection(Vector2[] directionOptions)
+    private Vector2Int ChooseDirection(Vector2Int[] directionOptions)
     {
         if (!directionOptions.Any())
             return PreviousDirectionOpposite;
@@ -87,24 +92,34 @@ public class AiMovement : MonoBehaviour
         return directionOptions[UnityEngine.Random.Range(0, directionOptions.Length)];
     }
 
-    private Vector2 MapPositionToDirection(Vector2 position) => position - CellPosition;
 
-    private bool IsPreviousDirectionAvailable(Vector2[] directionOptions) => directionOptions.Contains(_previousDirection);
-    private bool IsNewDirectionAvailable(Vector2[] directionOptions) => _previousDirectionOptions is null || directionOptions.Except(_previousDirectionOptions).Any();
-    private IEnumerable<Vector2> GetNeighboursOf(Vector2Int position)
+    private bool IsPreviousDirectionAvailable(Vector2Int[] directionOptions) => directionOptions.Contains(_previousDirection);
+    private bool IsNewDirectionAvailable(Vector2Int[] directionOptions) => _previousDirectionOptions is null || directionOptions.Except(_previousDirectionOptions).Any();
+    private Vector2Int GetDirectionToPosition(Vector3 position) => GetDirectionToCell(GetCell(position));
+    private Vector2Int GetDirectionToCell(Vector2Int cell) => cell - CellPosition;
+    private IEnumerable<Vector2Int> GetNeighbourCells(Vector3 position) => GetNeighbourCells(GetCell(position));
+    private IEnumerable<Vector2Int> GetNeighbourCells(Vector2Int cell)
     {
-        if (_allowedPositions.TryGetValue(position + Vector2Int.up, out Vector2Int upPosition))
+        if (_allowedPositions.TryGetValue(cell + Vector2Int.up, out Vector2Int upPosition))
             yield return upPosition;
-        if (_allowedPositions.TryGetValue(position + Vector2Int.down, out Vector2Int downPosition))
+        if (_allowedPositions.TryGetValue(cell + Vector2Int.down, out Vector2Int downPosition))
             yield return downPosition;
-        if (_allowedPositions.TryGetValue(position + Vector2Int.left, out Vector2Int leftPosition))
+        if (_allowedPositions.TryGetValue(cell + Vector2Int.left, out Vector2Int leftPosition))
             yield return leftPosition;
-        if (_allowedPositions.TryGetValue(position + Vector2Int.right, out Vector2Int rightPosition))
+        if (_allowedPositions.TryGetValue(cell + Vector2Int.right, out Vector2Int rightPosition))
             yield return rightPosition;
     }
+    private bool TryGetNeighbourCell(Vector2Int cell, Vector2Int direction, out Vector2Int neighbourCell)
+    {
+        return _allowedPositions.TryGetValue(cell + direction, out neighbourCell);
+    }
 
+    // TODO
     // WorldToCell appears to not give the right answer here
-    private Vector2Int ToVector2Int(Vector3 position) => new Vector2Int((int)Mathf.Round(position.x), (int)Mathf.Round(position.y));
+    //private Vector2Int GetCell(Vector3 position) => new Vector2Int((int)Mathf.Round(position.x), (int)Mathf.Round(position.y));
+    private Vector2Int GetCell(Vector3 position) => (Vector2Int)_allowedTiles.WorldToCell(position);
+    private Vector3 GetCellCentre(Vector2Int cell) => new Vector3(cell.x, cell.y, 0) + _centre.localPosition;
+
 
     [Serializable]
     private struct Movement
