0a551c82bded2cdd84eaea4ae7adfa9080e3255c
Chris Tilt <tiltchris+gh@gmail.com>
2023-10-07 16:28:05 +0100

    Add pulsing animation to collectables

---
+++ TypingGame/Assets/Ai/Collectable/CollectableAnimator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CollectableAnimator : MonoBehaviour
+{
+    [SerializeField] private AiMovement _movement;
+    [SerializeField] private Animator _animator;
+
+    private void Update()
+    {
+        _animator.SetBool("IsMoving", _movement.Direction != default);
+    }
+}
