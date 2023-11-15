a973ceebf6727503ff13bb528dca6db47914bb95
Chris Tilt <tiltchris+gh@gmail.com>
2023-11-15 20:26:31 +0000

    Add RandomUnit behaviour and random good and bad effect units

---
+++ TypingGame/Assets/Units/RandomUnit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RandomUnit : MonoBehaviour
+{
+    [SerializeField] private Unit[] _units;
+
+    private void Awake()
+    {
+        var randomIndex = Random.Range(0, _units.Length);
+        var unit = _units[randomIndex];
+        GameObject.Instantiate(unit, this.transform.position, this.transform.rotation, this.transform.parent);
+        GameObject.Destroy(this.gameObject);
+    }
+}
