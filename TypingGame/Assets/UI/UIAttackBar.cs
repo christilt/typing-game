4e6bf69fb6fd2aa5b5ab1c07e219b8e773821a0e
Chris Tilt <tiltchris+gh@gmail.com>
2024-02-04 21:46:05 +0000

    Improve visual of attack bar

--- TypingGame/Assets/UI/UIAttackBar.cs
+++ TypingGame/Assets/UI/UIAttackBar.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using DG.Tweening;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +12,11 @@ public class UIAttackBar : MonoBehaviour
 {
     [SerializeField] private Image _fill;
     [SerializeField] private GameObject _readyText;
+    [SerializeField] private Color _pulseFillColor;
+    [SerializeField] private float _pulseFillSeconds;
+    [SerializeField] private Ease _pulseFillEase;
+
+    private Tween _pulseWhiteTween;
 
     private float _fillWidth;
 
@@ -26,20 +33,25 @@ public class UIAttackBar : MonoBehaviour
 
     public void UpdateReadiness(PlayerAttackReadiness attackReadiness)
     {
-        if (attackReadiness.IsReady)
-        {
-            _readyText.SetActive(true);
-        }
-        else
-        {
-            _readyText.SetActive(false);
-        }
+        _readyText.gameObject.SetActive(attackReadiness.IsReady);
+        SetPulsing(attackReadiness.IsReady);
 
         // See https://stackoverflow.com/questions/30782829/how-to-access-recttransforms-left-right-top-bottom-positions-via-code
         // Right = -1 * offsetMax.x
         var x = GetFillRight(attackReadiness.Proportion) * -1f;
-        Debug.Log($"Attackbar x={x}");
         _fill.rectTransform.offsetMax = new Vector2(x, _fill.rectTransform.offsetMax.y);
+    }
+
+    private void SetPulsing(bool active)
+    {
+        _pulseWhiteTween?.Kill();
+
+        if (active)
+        {
+            _pulseWhiteTween = _fill.DOColor(_pulseFillColor, _pulseFillSeconds)
+                .SetEase(_pulseFillEase)
+                .SetLoops(-1, LoopType.Yoyo);
+        }
     }
 
     // If width = 400, 400 is 0% and 0 is 100%
