2b159b5aa90dd2fe7deb956533d4cf34d0108031
Chris Tilt <tiltchris+gh@gmail.com>
2024-07-28 19:03:51 +0100

    Simplify navigation with MenuPage monobehaviour

---
+++ TypingGame/Assets/UI/MenuPage.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPage : MonoBehaviour
+{
+    [SerializeField] private MenuPage _nextPage;
+    private MenuPage _previousPage;
+    private Button _firstButton;
+
+    private void OnEnable()
+    {
+        _firstButton ??= GetFirstButtonOrDefault();
+        if (_firstButton == null)
+        {
+            Debug.LogWarning("No buttons to select");
+            return;
+        }
+
+        _firstButton.Select();
+    }
+
+    public void OpenNext(MenuPage page)
+    {
+        if (page == null)
+        {
+            Debug.LogError("Page to open is null");
+            return;
+        }
+
+        _nextPage = page;
+
+        _nextPage.OpenFromPrevious(this);
+    }
+
+    public void OpenNext() => OpenNext(_nextPage);
+
+    public void OpenPrevious()
+    {
+        if (_previousPage == null)
+        {
+            Debug.LogError("Page to open is null");
+            return;
+        }
+
+        _previousPage.OpenFromNext();
+    }
+
+    private void OpenFromPrevious(MenuPage previous)
+    {
+        if (_previousPage == null)
+            _previousPage = previous;
+
+        gameObject.SetActive(true);
+
+        _previousPage.gameObject.SetActive(false);
+    }
+
+    private void OpenFromNext()
+    {
+        gameObject.SetActive(true);
+
+        _nextPage.gameObject.SetActive(false);
+    }
+
+    private Button GetFirstButtonOrDefault()
+    {
+        var childButtons = GetComponentsInChildren<Button>()
+            .Where(b => b.IsInteractable())
+            .OrderByDescending(b => b.transform.position.y) // top to bottom
+            .ThenBy(b => b.transform.position.x) // left to right
+            .ToArray();
+        if (childButtons.Length == 0)
+            return null;
+
+        return childButtons[0];
+    }
+}
