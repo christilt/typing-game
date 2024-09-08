1764f8d57a28dd87982dc04fa5d04ab3e7a034e4
Chris Tilt <tiltchris+gh@gmail.com>
2024-09-08 19:57:20 +0100

    Add text interpolation component

---
+++ TypingGame/Assets/UI/TextInterpolator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public abstract class TextInterpolator : MonoBehaviour   
+{
+    protected TextMeshProUGUI _text;
+
+    protected object[] _args;
+
+    protected void Awake()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        _args = GetTextArgs();
+    }
+
+    protected void OnEnable()
+    {
+        if (_args == null)
+            throw new ArgumentNullException(nameof(_args));
+
+        var format = _text.text;
+        _text.text = string.Format(format, _args); 
+    }
+
+    protected abstract object[] GetTextArgs();
+}
---
+++ TypingGame/Assets/UI/TextInterpolatorLevelStats.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class TextInterpolatorLevelStats : TextInterpolator
+{
+    protected override object[] GetTextArgs()
+    {
+        return new object[]
+        {
+            ColorUtility.ToHtmlStringRGB(new(255, 0, 0)),
+            ColorUtility.ToHtmlStringRGB(new(255, 255, 0)),
+            ColorUtility.ToHtmlStringRGB(new(255, 0, 255)),
+            0.986f,
+            new TimeSpan(0, 2, 15),
+            35,
+            40
+        };
+    }
+}
