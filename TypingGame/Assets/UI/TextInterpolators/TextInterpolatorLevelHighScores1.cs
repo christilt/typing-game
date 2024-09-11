11d85357a839216586263bd17998e96b7d1cc8b4
Chris Tilt <tiltchris+gh@gmail.com>
2024-09-11 21:46:52 +0100

    Improve arrangement of end of level display

--- TypingGame/Assets/UI/TextInterpolators/TextInterpolatorLevelHighScores1.cs
+++ TypingGame/Assets/UI/TextInterpolators/TextInterpolatorLevelHighScores1.cs
@@ -6,29 +6,9 @@ public class TextInterpolatorLevelHighScores1 : TextInterpolator
 {
     protected override object[] GetTextArgs()
     {
-        var levelId = LevelSettingsManager.Instance.LevelSettings.LevelId;
-        var levelHighScores = SaveDataManager.Instance.LoadLevelHighScores(levelId);
-
-        var whiteColourString = ColorUtility.ToHtmlStringRGB(Color.white);
-        var argsList = new List<object>();
-        argsList.Add(LevelSettingsManager.Instance.LevelSettings.LevelName);
-        for (var i = 0; i < 5; i++)
+        return new object[]
         {
-            if (levelHighScores.Count > i)
-            {
-                var highScore = levelHighScores[i];
-                argsList.Add(highScore.Initials);
-                argsList.Add(highScore.ColourHtmlString);
-                argsList.Add(highScore.Value);
-            }
-            else
-            {
-                argsList.Add("...");
-                argsList.Add(whiteColourString);
-                argsList.Add("...");
-            }
-        }
-
-        return argsList.ToArray();
+            LevelSettingsManager.Instance.LevelSettings.LevelName
+        };
     }
 }
